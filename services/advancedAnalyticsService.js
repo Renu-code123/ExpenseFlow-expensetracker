b6a2b5c0d5222567f43d3754a26dfa908f08520d
Gupta-02 <Gupta-02@users.noreply.github.com>
2026-01-25 08:57:41 +0530

    Implemented Navbar and fixed js handlers

---
+++ services/advancedAnalyticsService.js
@@ -0,0 +1,8 @@
+// Stub for advanced analytics service
+class AdvancedAnalyticsService {
+  init() {
+    console.log('Advanced analytics service initialized (stub)');
+  }
+}
+
+module.exports = new AdvancedAnalyticsService();
