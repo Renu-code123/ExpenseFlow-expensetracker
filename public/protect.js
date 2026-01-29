71d661fb8cd6886fccd217f8ba3958f6495c9fc2
omkar hole <omkarhole314@gmail.com>
2026-01-29 13:17:47 +0530

    created profile page
    closes: #403
    
    @Renu-code123  please review it

--- public/protect.js
+++ public/protect.js
@@ -1,7 +1,7 @@
 // protect.js to restrict access to authenticated users only for frontend pages
 
 (function(){
-    const token=localStorage.getItem('authToken');
+    const token=localStorage.getItem('token');
     if(!token){
         window.location.href='/login.html';
     }
